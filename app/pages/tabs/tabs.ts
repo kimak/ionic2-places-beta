77099f74f2e6618bc75ec633933a2a36dabc76cf
Kimak <mickael.dumand@gmail.com>
2016-07-04 19:37:41 +0200

    add logout button

--- app/pages/tabs/tabs.ts
+++ app/pages/tabs/tabs.ts
@@ -4,6 +4,7 @@ import { PlacesPage } from '../places/places';
 import { AddPage } from '../add/add';
 import { AroundPage } from '../around/around';
 import { FriendsPage } from '../friends/friends';
+import {Routes} from '../../providers/routes/routes'
 
 
 @Component({
@@ -16,7 +17,15 @@ export class TabsPage {
   tab3Root:any = AroundPage;
   tab4Root:any = FriendsPage;
 
-  constructor(private nav: NavController) {
+  /** Not normally mandatory but create bugs if ommited. **/
+  static get parameters() {
+        return [[NavController], [Routes]];
+  }
+  constructor(private nav: NavController, private routes:Routes) {
 
+  }
+
+  logout(){
+      this.nav.setRoot(this.routes.getRootPage())
   }
 }
