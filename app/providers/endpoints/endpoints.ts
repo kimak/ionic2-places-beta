67e82c528496fb26deb22795f3b28283c0e59494
Kimak <mickael.dumand@gmail.com>
2016-07-05 15:03:27 +0200

    update path with heroku

--- app/providers/endpoints/endpoints.ts
+++ app/providers/endpoints/endpoints.ts
@@ -9,7 +9,7 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class Endpoints {
 
-  API_PATH: string = "http://localhost:3001"
+  API_PATH: string = "http://ionic-places-jwt.herokuapp.com"
 
   getLogin(){
     return this.API_PATH + "/sessions/create"
