66a7938af2f968f46cd6cc94dc98ebf5023b16c8
Kimak <mickael.dumand@gmail.com>
2016-07-06 16:40:41 +0200

    remove useless platform ready

--- app/pages/add/add.ts
+++ app/pages/add/add.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import {Platform} from 'ionic-angular';
 import {Camera} from 'ionic-native';
 
 /*
@@ -16,25 +15,21 @@ export class AddPage {
 
   public base64Image: string;
 
-  constructor(private platform: Platform, private nav: NavController) {
+  constructor(private nav: NavController) {
 
   }
 
   getPicture(){
 
-      this.platform.ready().then(() => {
-        Camera.getPicture({
-          destinationType: Camera.DestinationType.DATA_URL,
-          targetWidth: 200,
-          targetHeight: 200
-        }).then((imageData) => {
-          this.base64Image = "data:image/jpeg;base64," + imageData;
-        }, (error) => {
-          console.log("error ",error)
-        });
-      })
-
-
+      Camera.getPicture({
+        destinationType: Camera.DestinationType.DATA_URL,
+        targetWidth: 200,
+        targetHeight: 200
+      }).then((imageData) => {
+        this.base64Image = "data:image/jpeg;base64," + imageData;
+      }, (error) => {
+        console.log("error ",error)
+      });
 
   }
 
